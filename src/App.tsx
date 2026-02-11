77012de3451abbc4cfce7adca138b104721a5ed6
TAMARTAM <ts0556726468@gmail.com>
2026-02-11 15:25:02 +0200

    Add all project files to main branch

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { FireCMSCloudApp } from "@firecms/cloud";
+import appConfig from "./index";
+
+function App() {
+    return <FireCMSCloudApp
+        projectId={"koren-stage-b"}
+        appConfig={appConfig}
+    />;
+}
+
+export default App
